910547d1b2a406a26c4f2f0adeabca0e4ac47a6f
handson1 <238203@unai.edu>
2024-10-01 10:08:24 +0700

    hansond5

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String[] todos = new String[3]
+    public static Scanner scanner = new Scanner(System.in)
+
+    public static void main(String[] args) {
+         addTodoList("Mewarnai");
+         addTodoList("")
+    }
+}
